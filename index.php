7c828d07e8ed91fee3b1dc615a2c3d1f25b82d6d
Juan Jose Jauregui Acosta <jauregui-94@hotmail.com>
2020-10-16 14:23:51 -0500

    nuevo

--- index.php
+++ index.php
@@ -3,10 +3,15 @@
         <head>
             <meta charset="UTF-8" />
             <title>Tienda de Camisetas</title>
+            <link rel="stylesheet" href="assets/css/styles.css">
         </head>
         <body>
             <!-- CABECERA -->
             <header id="header">
+                <div id="logo">
+                    <img src="assets/img/camiseta.png" alt="Camiseta Logo" />
+                    <a href="index.php">Tienda de camisetas</a>
+                </div>
             </header>
             <!-- MENU -->
             <!-- BARRA LATERAL -->
