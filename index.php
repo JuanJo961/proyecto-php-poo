71a03a25025345c068a830fd5dc6990c850e5685
Juan Jose Jauregui Acosta <jauregui-94@hotmail.com>
2020-10-16 13:23:09 -0500

    nuevo

---
+++ index.php
@@ -0,0 +1,18 @@
+<DOCTYPE HTML>
+    <html lang="es">
+        <head>
+            <meta charset="UTF-8" />
+            <title>Tienda de Camisetas</title>
+        </head>
+        <body>
+            <!-- CABECERA -->
+            <header id="header">
+            </header>
+            <!-- MENU -->
+            <!-- BARRA LATERAL -->
+            <!-- CONTENIDO CENTRAL -->
+            <!-- FOOTER -->
+
+        </body>
+    </html>
+</DOCTYPE>
